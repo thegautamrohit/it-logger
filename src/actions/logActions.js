d66affc732f8ef04f517902d3c237e725a566396
Rohit Gautam <rgautam1997@gmail.com>
2021-05-02 19:23:40 +0530

    Initial Commit

---
+++ src/actions/logActions.js
@@ -0,0 +1,63 @@
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from "./types";
+
+// export const getLogs = () => {
+//   return async (dispatch) => {
+//     setLoading();
+
+//     const res = await fetch("/logs");
+//     const data = await res.json();
+
+//     dispatch({
+//       type: GET_LOGS,
+//       payload: data,
+//     });
+//   };
+// };
+
+//Get logs from server
+export const getLogs = () => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch("/logs");
+    const data = await res.json();
+
+    dispatch({
+      type: GET_LOGS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    });
+  }
+};
+
+//Add new Log
+export const addLog = (log) => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch("/logs", {
+      method: "POST",
+      body: JSON.stringify(log),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_LOG,
+      payload: data,
+    });
+  } catch (error) {}
+};
+
+//Set Loading to true
+export const setLoading = () => {
+  return {
+    type: SET_LOADING,
+  };
+};
